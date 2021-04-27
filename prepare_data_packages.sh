6d8718965c3154ed65af7e323ef658ed831b312b
Mostafa ElAraby <4073898+mostafaelaraby@users.noreply.github.com>
2021-04-27 15:14:56 +0200

    added bash script data + dependency prepa.

---
+++ prepare_data_packages.sh
@@ -0,0 +1,12 @@
+wget https://www.dropbox.com/s/kp3my3412u5k9rl/Imagenet_resize.tar.gz?dl=1 --output-document=Imagenet_resize.tar.gz
+wget https://www.dropbox.com/s/moqh2wh8696c3yl/LSUN_resize.tar.gz?dl=1 --output-document=LSUN_resize.tar.gz
+mkdir -p data/
+tar -xf Imagenet_resize.tar.gz -C data/
+tar -xf LSUN_resize.tar.gz -C data/
+
+echo "Installing packages"
+pip3 install -r requirements.txt
+git clone https://www.github.com/lebrice/Sequoia.git
+cd Sequoia
+pip3 install -e .
+
